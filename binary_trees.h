8df0f1087c1b4dd36a28d2de271d257d7dec5955
Zac <13089@holbertonstudents.com>
2026-04-01 00:41:01 +0000

    fix

--- binary_trees.h
+++ binary_trees.h
@@ -22,4 +22,7 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
+/* function prototypes */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
 #endif
